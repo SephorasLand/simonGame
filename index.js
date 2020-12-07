683548bb5ec7f9c8ab5de413a4d56fb4b24b3727
Raquel Pereda <rperedah@gmail.com>
2020-12-07 15:38:21 +0100

    added functionality of the game: show a pattern for the user to follow and check if the user got it right

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var hasBeenPressed = false;
+var level = 0;
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
@@ -8,19 +10,39 @@ function nextSequence(){
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+  $("h1").text("Level " + level);
+  level++;
+  userClickedPattern = [];
 }
 
 $(document).keypress(function(){
-  nextSequence();
+  if (!hasBeenPressed){
+    nextSequence();
+    hasBeenPressed = true;
+    $("h1").text("Level " + level);
+  }
 })
 
 
 $(".btn").on("click", function(event){
   var userChosenColour = event.target.id;
   userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
   animatePress(userChosenColour);
+  playSound(userChosenColour);
 })
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if(gamePattern.length === userClickedPattern.length){
+      setTimeout(nextSequence,1000);
+    }
+  } else {
+    playSound(wrong);
+    $("h1").text("Game over");
+    $("body").addClass("game-over");
+  }
+}
 
 function playSound(input){
   var simonMusic = new Audio("sounds/" + input + ".mp3");
